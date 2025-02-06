fbc294b13df23e12f3e797a933c8ccdd3a710224
marchelucie <lucie.pouponneau@live.fr>
2025-02-06 11:25:04 +0100

    page d'accueil terminée
    filtres et galerie dynamiques

--- FrontEnd/app.mjs
+++ FrontEnd/app.mjs
@@ -1,13 +1,13 @@
-// Appel API
+// ********** Appel API **********
 
 const response = await fetch("http://localhost:5678/api/works");
 const works = await response.json();
 console.log(works);
 
-// Gallerie interactive
+// ********** Galerie interactive **********
 const gallery = document.querySelector(".gallery");
 
-function displayWorks() {
+function displayWorks(works) {
     gallery.innerText = "";
     for (let i = 0; i < works.length; i++) {
         const divElement = document.createElement("div");
@@ -19,14 +19,34 @@ function displayWorks() {
         divElement.appendChild(imageElement);
         divElement.appendChild(titleElement);
     }
-}
+};
 
-displayWorks();
+displayWorks(works);
 
-// Ajout dynamique et auto des filtres
-// ***** Choix fait pour pouvoir ajouter automatiquement un filtre si une nouvelle catégorie est créée pour un work
+// **********Ajout dynamique et auto des filtres**********
+// -- Choix fait pour pouvoir ajouter automatiquement un filtre si une nouvelle catégorie est créée pour un work --
 
-function displayFilters() {
+//Fonction d'automatisation des filtres
+function filterWorks(categoryId, works){
+    gallery.innerHTML = "";
+    const worksFiltered = works.filter(function (work) {
+        return work.categoryId === categoryId
+    });
+    console.log("Filtrés :", worksFiltered)
+    for (let i = 0; i < worksFiltered.length; i++) {
+        const divElement = document.createElement("div");
+        gallery.appendChild(divElement);
+        const imageElement = document.createElement("img");
+        imageElement.src = worksFiltered[i].imageUrl;
+        const titleElement = document.createElement("p");
+        titleElement.innerText = `${worksFiltered[i].title}`;
+        divElement.appendChild(imageElement);
+        divElement.appendChild(titleElement);
+    }
+};
+
+// Fonction d'apparition auto des filtres
+function displayFilters(works) {
     const filters = document.querySelector(".filters");
     const setFilters = new Set();
     for (let i = 0; i < works.length; i++) {
@@ -34,85 +54,23 @@ function displayFilters() {
             setFilters.add(works[i].category.name);
             const btnFilter = document.createElement("button");
             btnFilter.innerText = works[i].category.name;
-            btnFilter.id = `${works[i].category.id}`;
+            btnFilter.id = works[i].category.id;
+            btnFilter.addEventListener("click", () => filterWorks(Number(btnFilter.id), works)); //Appel du listener pour automatisation des filtres
             filters.appendChild(btnFilter);
         }
-    }
+    };
 };
 
-displayFilters();
-
-// Ajout des listener pour filtres
-
-const btnFilterAll = document.getElementById("all");
-const btnFilterObjects = document.getElementById("1");
-const btnFilterApt = document.getElementById("2");
-const btnFilterHotels = document.getElementById("3");
+displayFilters(works); //Appel de filterWorks() à l'intérieur de displayWorks()
 
 // Filtre "Tous"
+const btnFilterAll = document.getElementById("all");
 btnFilterAll.addEventListener("click", function () {
     gallery.innerHTML = "";
-    const worksFiltered = works.filter(function (work) {
+    const worksFiltered = works.filter(function () {
         return works
     });
-    console.log(worksFiltered);
-    for (let i = 0; i < worksFiltered.length; i++) {
-        const divElement = document.createElement("div");
-        gallery.appendChild(divElement);
-        const imageElement = document.createElement("img");
-        imageElement.src = worksFiltered[i].imageUrl;
-        const titleElement = document.createElement("p");
-        titleElement.innerText = `${worksFiltered[i].title}`;
-        divElement.appendChild(imageElement);
-        divElement.appendChild(titleElement);
-    }
-});
-
-// Filtre Objets
-btnFilterObjects.addEventListener("click", function () {
-    gallery.innerHTML = "";
-    const worksFiltered = works.filter(function (work) {
-        return work.categoryId === `${work.category.id}`
-    });
-    console.log(worksFiltered)
-    for (let i = 0; i < worksFiltered.length; i++) {
-        const divElement = document.createElement("div");
-        gallery.appendChild(divElement);
-        const imageElement = document.createElement("img");
-        imageElement.src = worksFiltered[i].imageUrl;
-        const titleElement = document.createElement("p");
-        titleElement.innerText = `${worksFiltered[i].title}`;
-        divElement.appendChild(imageElement);
-        divElement.appendChild(titleElement);
-    }
-});
-
-// Filtre Appartements
-btnFilterApt.addEventListener("click", function () {
-    gallery.innerHTML = "";
-    const worksFiltered = works.filter(function (work) {
-        return work.categoryId === 2
-    });
-    console.log(worksFiltered)
-    for (let i = 0; i < worksFiltered.length; i++) {
-        const divElement = document.createElement("div");
-        gallery.appendChild(divElement);
-        const imageElement = document.createElement("img");
-        imageElement.src = worksFiltered[i].imageUrl;
-        const titleElement = document.createElement("p");
-        titleElement.innerText = `${worksFiltered[i].title}`;
-        divElement.appendChild(imageElement);
-        divElement.appendChild(titleElement);
-    }
-});
-
-// Filtre Hotels & restaurants
-btnFilterHotels.addEventListener("click", function () {
-    gallery.innerHTML = "";
-    const worksFiltered = works.filter(function (work) {
-        return work.categoryId === 3
-    });
-    console.log(worksFiltered)
+    console.log("Filtrés :", worksFiltered);
     for (let i = 0; i < worksFiltered.length; i++) {
         const divElement = document.createElement("div");
         gallery.appendChild(divElement);
